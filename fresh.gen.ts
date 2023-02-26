99add4138c01dfd3038544475e2ccf7270c05466
fitzypop <32967490+fitzypop@users.noreply.github.com>
2023-02-26 02:25:53 -0700

    update manifest

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,15 +3,31 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/api/film/[slug].ts";
-import * as $1 from "./routes/api/films.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $0 from "./routes/api/films/[slug].ts";
+import * as $1 from "./routes/api/films/index.ts";
+import * as $2 from "./routes/api/locations/[slug].ts";
+import * as $3 from "./routes/api/locations/index.ts";
+import * as $4 from "./routes/api/people/[slug].ts";
+import * as $5 from "./routes/api/people/index.ts";
+import * as $6 from "./routes/api/species/[slug].ts";
+import * as $7 from "./routes/api/species/index.ts";
+import * as $8 from "./routes/api/vehicles/[slug].ts";
+import * as $9 from "./routes/api/vehicles/index.ts";
+import * as $10 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
-    "./routes/api/film/[slug].ts": $0,
-    "./routes/api/films.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/api/films/[slug].ts": $0,
+    "./routes/api/films/index.ts": $1,
+    "./routes/api/locations/[slug].ts": $2,
+    "./routes/api/locations/index.ts": $3,
+    "./routes/api/people/[slug].ts": $4,
+    "./routes/api/people/index.ts": $5,
+    "./routes/api/species/[slug].ts": $6,
+    "./routes/api/species/index.ts": $7,
+    "./routes/api/vehicles/[slug].ts": $8,
+    "./routes/api/vehicles/index.ts": $9,
+    "./routes/index.tsx": $10,
   },
   islands: {},
   baseUrl: import.meta.url,
