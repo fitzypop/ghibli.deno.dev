fa3c5d75178856853a74304edf0585f8b8b2fa44
fitzypop <32967490+fitzypop@users.noreply.github.com>
2023-02-27 15:25:41 -0700

    remove log

--- routes/api/locations/[slug].ts
+++ routes/api/locations/[slug].ts
@@ -3,7 +3,6 @@ import { controller } from "@/utils/controller.ts";
 
 export const handler: Handlers = {
   async GET(_req, ctx) {
-    console.log(new URL(_req.url));
     return await controller("locations", ctx);
   },
 };
