5ffdcf8b97c6c4d50e5f55c8735eefae102bb311
fitzypop <32967490+fitzypop@users.noreply.github.com>
2023-02-26 22:03:12 -0700

    basic error logging

--- routes/api/films/index.ts
+++ routes/api/films/index.ts
@@ -4,6 +4,7 @@ import { supabase } from "../../../utils/supabase.ts";
 export const handler: Handlers = {
   async GET(req, ctx) {
     const { data, error } = await supabase.from("films").select();
+    if (error) console.error(error);
     return new Response(JSON.stringify(data));
   },
 };
